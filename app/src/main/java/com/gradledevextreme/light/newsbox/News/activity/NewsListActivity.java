f18e520d626972657cb326ad32e93a6702ff41f6
Amit <ajaiswal@teksystems.com>
2018-08-06 20:55:40 +0530

    removed usage of libraries

--- app/src/main/java/com/gradledevextreme/light/newsbox/News/activity/NewsListActivity.java
+++ app/src/main/java/com/gradledevextreme/light/newsbox/News/activity/NewsListActivity.java
@@ -3,7 +3,6 @@ package com.gradledevextreme.light.newsbox.News.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -39,6 +38,7 @@ public class NewsListActivity extends BaseActivity {
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_URL = "url";
     public static final String KEY_PUBLISHEDAT = "publishedAt";
+    String data = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,77 +49,35 @@ public class NewsListActivity extends BaseActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
         listNews.setEmptyView(progressBar);
 
-        if(isNetworkAvailable(getApplicationContext()))
-        {
-            DownloadNews newsTask = new DownloadNews();
-            newsTask.execute();
-        }else{
+        if (isNetworkAvailable(getApplicationContext())) {
+
+            //creating new thread
+            new Thread(new Runnable() {
+                public void run() {
+
+                    data = "";
+                    String urlParameters = "";
+                    data = getData("https://newsapi.org/v1/articles?source=" + NEWS_SOURCE + "&sortBy=top&apiKey=" + API_KEY, urlParameters);
+
+
+                    NewsListActivity.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            runUI();
+                        }
+                    });
+                }
+            }).start();
+
+        } else {
             Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
         }
 
-    }
-
-
-    class DownloadNews extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-        }
-
-        protected String doInBackground(String... args) {
-            String data = "";
-
-            String urlParameters = "";
-            data = excuteGet("https://newsapi.org/v1/articles?source=" + NEWS_SOURCE + "&sortBy=top&apiKey=" + API_KEY, urlParameters);
-            return data;
-        }
-
-        @Override
-        protected void onPostExecute(String data) {
-
-            if (data.length() > 20) {
-
-                try {
-                    JSONObject jsonResponse = new JSONObject(data);
-                    JSONArray jsonArray = jsonResponse.optJSONArray("articles");
-
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-                        HashMap<String, String> map = new HashMap<String, String>();
-                        map.put(KEY_AUTHOR, jsonObject.optString(KEY_AUTHOR).toString());
-                        map.put(KEY_TITLE, jsonObject.optString(KEY_TITLE).toString());
-                        map.put(KEY_DESCRIPTION, jsonObject.optString(KEY_DESCRIPTION).toString());
-                        map.put(KEY_URL, jsonObject.optString(KEY_URL).toString());
-                        map.put(KEY_PUBLISHEDAT, jsonObject.optString(KEY_PUBLISHEDAT).toString());
-                        dataList.add(map);
-                    }
-                } catch (JSONException e) {
-                    Toast.makeText(getApplicationContext(), "Unexpected error", Toast.LENGTH_SHORT).show();
-                }
-
-                ListNewsAdapter adapter = new ListNewsAdapter(NewsListActivity.this, dataList);
-                listNews.setAdapter(adapter);
-
-                listNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    public void onItemClick(AdapterView<?> parent, View view,
-                                            int position, long id) {
-                        Intent i = new Intent(NewsListActivity.this, WebViewDetailActivity.class);
-                        i.putExtra("url", dataList.get(+position).get(KEY_URL));
-                        startActivity(i);
-                    }
-                });
-
-            } else {
-                Toast.makeText(getApplicationContext(), "No news found", Toast.LENGTH_SHORT).show();
-            }
-        }
-
 
     }
 
-    public static String excuteGet(String targetURL, String urlParameters) {
+
+    // to get json data
+    public static String getData(String targetURL, String urlParameters) {
         URL url;
         HttpURLConnection connection = null;
         try {
@@ -163,11 +121,48 @@ public class NewsListActivity extends BaseActivity {
         }
     }
 
-    public static boolean isNetworkAvailable(Context context)
-    {
+    public static boolean isNetworkAvailable(Context context) {
         return ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo() != null;
     }
 
+    //task to run on UI
+    public void runUI() {
+        if (data.length() > 20) {
+
+            try {
+                JSONObject jsonResponse = new JSONObject(data);
+                JSONArray jsonArray = jsonResponse.optJSONArray("articles");
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    map.put(KEY_AUTHOR, jsonObject.optString(KEY_AUTHOR).toString());
+                    map.put(KEY_TITLE, jsonObject.optString(KEY_TITLE).toString());
+                    map.put(KEY_DESCRIPTION, jsonObject.optString(KEY_DESCRIPTION).toString());
+                    map.put(KEY_URL, jsonObject.optString(KEY_URL).toString());
+                    map.put(KEY_PUBLISHEDAT, jsonObject.optString(KEY_PUBLISHEDAT).toString());
+                    dataList.add(map);
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Unexpected error", Toast.LENGTH_SHORT).show();
+            }
+
+            ListNewsAdapter adapter = new ListNewsAdapter(NewsListActivity.this, dataList);
+            listNews.setAdapter(adapter);
+
+            listNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    Intent i = new Intent(NewsListActivity.this, WebViewDetailActivity.class);
+                    i.putExtra("url", dataList.get(+position).get(KEY_URL));
+                    startActivity(i);
+                }
+            });
+
+        } else {
+            Toast.makeText(getApplicationContext(), "No news found", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
 }
